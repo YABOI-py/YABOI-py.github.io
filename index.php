b9133bd4fcef7d4cbbaf1a1eca460f0561553238
YABOI <89107550+YABOI-py@users.noreply.github.com>
2022-03-26 22:53:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 <body>
 
 <?php
-//If your Discord.php file is in another folder you need to change the file path!
-include("Discord.php");
+
+include("log.php");
 $sendembed = New Discord();
 
-//Executes the function
+
 $sendembed->Visitor();
 ?>
 
